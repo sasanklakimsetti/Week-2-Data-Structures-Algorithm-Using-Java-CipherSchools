a81ee8136d17409189a86467ce77ad466dc6d17a
sasanklakimsetti <saisasank.lakimsetti@gmail.com>
2024-03-17 15:44:01 +0530

    Class 14

--- Class-14/MutlipleInheritanceInterfaceStudy.java
+++ Class-14/MutlipleInheritanceInterfaceStudy.java
@@ -7,10 +7,26 @@ interface B{
     void fun();
 }
 class C implements A,B{
-    
+    public void fun(){
+        System.out.println("I'm function in C");
+        System.out.println(A.x);
+        System.out.println(B.x);
+    }
+}
+//multiple inheritance through interfaces
+interface D extends A,B{
+    void fun();
+}
+class E implements D{
+    public void fun(){
+        System.out.println("I'm function in D");
+    }
 }
 public class MutlipleInheritanceInterfaceStudy {
     public static void main(String[] args){
-
+        C c=new C();
+        c.fun();
+        E d=new E();
+        d.fun();
     }
 }
