9a0abc2b4c22ab2a8477c6fa649b107a30d4eca8
sasanklakimsetti <saisasank.lakimsetti@gmail.com>
2024-03-17 15:32:00 +0530

    Class 14

---
+++ Class-14/MutlipleInheritanceInterfaceStudy.java
@@ -0,0 +1,16 @@
+interface A{
+    int x=10;
+    void fun();
+}
+interface B{
+    int x=20;
+    void fun();
+}
+class C implements A,B{
+    
+}
+public class MutlipleInheritanceInterfaceStudy {
+    public static void main(String[] args){
+
+    }
+}
